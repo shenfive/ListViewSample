e3f51d9067cc4584bb67b49f3146cbeafde09e38
Danny Shen <shenfive@gmail.com>
2024-08-27 18:04:18 +0800

    Initial Commit

---
+++ ListViewSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ListViewSample
+//
+//  Created by 申潤五 on 2024/8/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ListViewSample/ListViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewSampleApp.swift
+//  ListViewSample
+//
+//  Created by 申潤五 on 2024/8/27.
+//
+
+import SwiftUI
+
+@main
+struct ListViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
